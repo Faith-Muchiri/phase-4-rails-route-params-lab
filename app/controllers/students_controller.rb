55c5ffe0d1137fe0c02989e243ea36f4d794b59c
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-11-21 17:45:28 +0300

    complete tests

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def each
+    individual_student = Student.find_by(id: params[:id])
+    render json: individual_student   
+  end
+
 end
